106421ba1f40650aa3ce9472f4d88f2eec5aff62
marco <toyko20012gmail.com>
2023-09-22 10:07:30 +0630

    fixed -- ARCH name

--- rust/flat/build.sh
+++ rust/flat/build.sh
@@ -4,9 +4,9 @@ cargo build --release --target i686-linux-android  # x86
 
 mkdir -p ../../android/app/src/main/jniLibs
 mkdir -p ../../android/app/src/main/jniLibs/arm64-v8a/
-mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7/
+mkdir -p ../../android/app/src/main/jniLibs/armeabi-v7a/
 mkdir -p ../../android/app/src/main/jniLibs/x86/
 
 cp ./target/aarch64-linux-android/release/libflat.so ../../android/app/src/main/jniLibs/arm64-v8a/
-cp ./target/armv7-linux-androideabi/release/libflat.so ../../android/app/src/main/jniLibs/armeabi-v7/
+cp ./target/armv7-linux-androideabi/release/libflat.so ../../android/app/src/main/jniLibs/armeabi-v7a/
 cp ./target/i686-linux-android/release/libflat.so ../../android/app/src/main/jniLibs/x86/
